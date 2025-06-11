a0783d033aa158a47b243a5533e5d08bb9608d67
judeprod <judeprodi02@gmail.com>
2025-06-11 22:55:06 +0800

    SignUpcontroller

---
+++ KapIt/src/main/java/com/example/SignUpController.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SignUpController {
+
+    @FXML
+    private void signupbtnHandler(ActionEvent event) throws IOException {
+        App.setRoot("register"); 
+    }
+
+    @FXML
+    private void loginbtnHandler(ActionEvent event) throws IOException {
+        App.setRoot("LoginMoveIt"); 
+    }
+     @FXML
+    private void nextbtnHandler(ActionEvent event) throws IOException {
+       App.setRoot("LoginMoveit"); // Replace "nextScreen" with your actual next FXML file name (without .fxml)
+    }
+}
+
